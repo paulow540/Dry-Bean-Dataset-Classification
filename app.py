b33f00a12420bdd2324458456599df56792ac1d8
paulow540 <halleluyahpaul@gmail.com>
2023-04-17 20:41:00 +0530

    i added some function when the prediction is showing error

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template, request,jsonify
+from flask import Flask , render_template, request,jsonify, url_for
 import pickle
 import os
 
@@ -12,82 +12,74 @@ import numpy as np
 app = Flask(__name__)
 model = pickle.load(open("model.pkl", "rb"))
 app.config["UPLOAD_FOLDER"] = picfolder
-@app.route("/", methods = ['GET', 'POST'])
-def hello():
+@app.route("/predict", methods = ['GET', 'POST'])
+def predict():
+   
     int_f = [float(x) for x in request.form.values()]
     fina = np.array(int_f)
     myfin = fina[np.newaxis, :]
-    prediction = model.predict(myfin)
-    # print(fina, "arrrrr")
-    print(myfin, type(myfin), myfin.ndim) 
-    # 'SEKER', 'BARBUNYA', 'BOMBAY', 'CALI', 'HOROZ', 'SIRA', 'DERMASON'
-    if prediction[0] == "'SEKER'":
-        print("'SEKER'")
-        pic1 = os.path.join(app.config["UPLOAD_FOLDER"], "seker bean.jpg")
-    elif prediction[0] == 'BARBUNYA':
-        print("BARBUNYA")
-        
-        pic1 = os.path.join(app.config["UPLOAD_FOLDER"], "BARBUNYA bean.jpg")
-
-    elif prediction[0] ==  'BOMBAY':
-        pic1 = os.path.join(app.config["UPLOAD_FOLDER"], "BOMBAY.jpg")
-
-        print('BOMBAY')
-    elif prediction[0] == 'CALI':
-        pic1 = os.path.join(app.config["UPLOAD_FOLDER"], "CALI bean.jpg")
-
-        print('CALI')
-    elif prediction[0] == 'HOROZ':
-        pic1 = os.path.join(app.config["UPLOAD_FOLDER"], "HOROZ bean.jpg")
-
-        print('HOROZ')
-    elif prediction[0] == 'SIRA':
-        pic1 = os.path.join(app.config["UPLOAD_FOLDER"], "SIRA bean.jpg")
-
-        print('SIRA')
+    print(myfin, type(myfin))
+    if myfin == "[]":
+        pass
+      
     else:
-        pic1 = os.path.join(app.config["UPLOAD_FOLDER"], "DERMASON beans.jpg")
+        prediction = model.predict(myfin)
+        # print(myfin, "arrrrr")
+        # print(myfin, type(myfin), myfin.ndim) 
+        # 'SEKER', 'BARBUNYA', 'BOMBAY', 'CALI', 'HOROZ', 'SIRA', 'DERMASON'
+        if prediction[0] == "SEKER":
+            print("'SEKER'")
+            pic1 = os.path.join(app.config["UPLOAD_FOLDER"], "seker bean.jpg")
+        elif prediction[0] == 'BARBUNYA':
+            print("BARBUNYA")
+            
+            pic1 = os.path.join(app.config["UPLOAD_FOLDER"], "BARBUNYA bean.jpg")
 
-        print('DERMASON')
+        elif prediction[0] ==  'BOMBAY':
+            pic1 = os.path.join(app.config["UPLOAD_FOLDER"], "BOMBAY.jpg")
 
-    return render_template("index.html", my_ourbeans=f"The Name of the Beans is {prediction[0]}", pic=pic1)
+            print('BOMBAY')
+        elif prediction[0] == 'CALI':
+            pic1 = os.path.join(app.config["UPLOAD_FOLDER"], "CALI bean.jpg")
+
+            print('CALI')
+        elif prediction[0] == 'HOROZ':
+            pic1 = os.path.join(app.config["UPLOAD_FOLDER"], "HOROZ bean.jpg")
+
+            print('HOROZ')
+        elif prediction[0] == 'SIRA':
+            pic1 = os.path.join(app.config["UPLOAD_FOLDER"], "SIRA bean.jpg")
+
+            print('SIRA')
+        else:
+            pic1 = os.path.join(app.config["UPLOAD_FOLDER"], "DERMASON beans.jpg")
+
+            print('DERMASON') 
+        
+        return render_template("predict.html", my_ourbeans=f"The Name of the Beans is {prediction[0]}" , pic=myfin)
+
+       
+    
+
+    
+
+@app.route("/", methods = ['GET', 'POST'])
+def homepage():
+    pic1 = os.path.join(app.config["UPLOAD_FOLDER"], "all bearns.jpg")
+
+    
+    return render_template("homepage.html",mypic=pic1)
 
 
 
-# @app.route("/sub", methods = ["POST"])
-# def submit():
-#     if request.method == "POST":
-#         name = request.form["username"]
-
-
-#     return render_template("sub.html", myname = name)
-
-
-  # if request.method == "POST":
-        # area = request.form["Area"]
-        # perimeter = request.form["Perimeter"]
-        # majorAxisLength = request.form["MajorAxisLength"]
-        # minorAxisLength = request.form["MinorAxisLength"]
-        # aspectRation = request.form["AspectRation"]
-        # eccentricity = request.form["Eccentricity"]
-        # convexArea = request.form["ConvexArea"]
-        # equivDiameter = request.form["EquivDiameter"]
-        # extent = request.form["Extent"]
-        # solidity = request.form["Solidity"]
-        # roundness = request.form["roundness"]
-        # compactness = request.form["Compactness"]
-        # shapeFactor1 = request.form["ShapeFactor1"]
-        # ShapeFactor2 = request.form["ShapeFactor2"]
-        # ShapeFactor3 = request.form["ShapeFactor3"]
-        # ShapeFactor4 = request.form["ShapeFactor4"]
-
-        # colum = np.array([area,perimeter,majorAxisLength,minorAxisLength,aspectRation,eccentricity,
-        #          convexArea,equivDiameter,extent,solidity,roundness,compactness,shapeFactor1,
-        #          ShapeFactor2,ShapeFactor3,ShapeFactor4])
-
-        # ourbeans = my.beans_prediction(colum)
-        # print(colum, "this columns")
-        # print(ourbeans, "alllllllllllll")
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
+
+
+
+
--- mymodel.py
+++ mymodel.py
@@ -10,29 +10,19 @@ import seaborn as sns
 # def beans_prediction(beans):
 beansdata = pd.read_excel("Dry_Bean_Dataset.xlsx")
 
-X_iris = beansdata.drop('Class', axis=1)
+X_iris = beansdata.drop(['Class',"Area","MajorAxisLength","MinorAxisLength",
+                        "AspectRation","Eccentricity","EquivDiameter","ShapeFactor1","ShapeFactor2","ShapeFactor3",
+                        "ShapeFactor4","ConvexArea"], axis=1)
 y_iris = beansdata["Class"]
 
 from sklearn.model_selection import train_test_split
 Xtrain, Xtest, ytrain, ytest = train_test_split(X_iris, y_iris, random_state=1)
 
-print(Xtrain.shape, "sdfghjfghjhj")
+print(Xtrain.shape, "sdfghjfghjhj", ytrain.shape)
+print(Xtest.shape, "testttttttt", ytest.shape)
 from sklearn.naive_bayes import GaussianNB # 1. choose model class
 modelGau = GaussianNB() # 2. instantiate model
 modelGau.fit(Xtrain, ytrain) # 3. fit model to data
 pickle.dump(modelGau, open("model.pkl","wb"))
 model = pickle.load(open("model.pkl", "rb"))
 
-# y_model = model.predict(Xtest)
-# print(y_model,"okkkkkkkkkkkkkkkkkkkk")
-
-# from sklearn.metrics import accuracy_score
-# accuracy_score(ytest, y_model)
-# print(accuracy_score(ytest, y_model), "accuracy_score#####")
-# new_sample = Xtest.copy() 
-# new_sample['species'] = ytest
-# new_sample.iloc[16]
-# y_model = model.predict(Xtest.iloc[16].values.reshape(1,16))
-# print("Your new specie is ",y_model[0], " flower.")
-
-# return model.predict(y_model)
